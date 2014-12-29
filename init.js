243c5ae15e5244e331acc3e170de7d22ee5fe566
Daniel Husar <dano.husar@gmail.com>
2014-12-29 15:00:45 +0000

    main logic

--- init.js
+++ init.js
@@ -3,9 +3,42 @@
 
   var $ = window.jQuery;
 
-  function init () {
+  function popup (url) {
+    $.colorbox({
+      href: url,
+      iframe: true,
+      scalePhotos: true,
+      transition: 'elastic',
+      opacity: 0.80,
+      innerWidth: 550,
+      innerHeight: 400
+    });
   }
 
-  init();
+  function init () {
+    $.colorbox.remove();
+
+    $('.titulkydownloadajax').click(function (e) {
+      e.preventDefault();
+
+      var $this = $(this);
+      var url = $this.attr('href');
+
+      $.get(url, function (data) {
+        var html = (new window.DOMParser()).parseFromString(data, 'text/xml'); //jQuery 1.4.2 in the document :(
+        var subtitles = $(html).find('#downlink').attr('href');
+
+        if (subtitles) {
+          window.location.href = subtitles;
+        } else {
+          popup(url);
+        }
+      });
+    });
+  }
+
+  if ($.colorbox) {
+    init();
+  }
 
 })(this, this.document);
--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
-# WIP
+# Titulky.com chrome extension
+
+> Remove annoying countdown in the popup until you can download subtitles
+
+
+## Installation
+
+Load it as unpacked extension. (I'm not going to submit it to chrome store)
+
+## What it does
+
+If you haven't reached 25 downloads per day, subtitles will be auto downloaded after clicking on the link.
+If you have reached 25 downloads per day, popup with captcha will appear, but after filling captcha you have no longer to wait few seconds and can directly download subtitles.
+
+
+## License
+
+MIT © Daniel Husar
